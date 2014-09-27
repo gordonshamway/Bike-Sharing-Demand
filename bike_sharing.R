88becf05133b3d20eff435a45c3ea0ab4367346d
gordonshamway <stef.buchholz@googlemail.com>
2014-09-27 20:23:28 +0200

    tried different models

--- bike_sharing.R
+++ bike_sharing.R
@@ -29,24 +29,44 @@ missmap(train.raw, main="Bike Sharing Data - Missing Values", col=c("yellow", "b
 
 #extract the hour and weekday of the date
 train.raw$hour <- hour(train.raw$datetime)
+train.raw$year <- year(train.raw$datetime)
+train.raw$month <- month(train.raw$datetime)
 train.raw$weekday <- weekdays(train.raw$datetime)
 train.raw$weekday <- as.factor(train.raw$weekday)
 train.raw$combined <- paste(as.character(train.raw$weekday), "_", as.character(train.raw$hour), sep="")
 train.raw$combined <- as.factor(train.raw$combined)
 
-
 #Modeling
 library(caret)
-rpart.model1 <- train(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + hour + as.factor(weekday), data=train.raw, method="rpart")
-rpart.model2 <- train(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + as.factor(combined), data=train.raw, method="rpart")
-glm.model1 <- train(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + hour + as.factor(weekday), data=train.raw, method="glm")
-glm.model2 <- train(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + combined, data=train.raw, method="glm")
-glm.model3 <- glm(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + combined, data=train.raw, family="poisson")
-glm.model4 <- glm(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + combined, data=train.raw, family="Gamma")
-glm.model99 <- train(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + combined, data=train.raw, method = "lm", family="Gamma", preProcess=c("center", "scale"))
-
 library(randomForest)
-rf.model1 <- randomForest(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + hour + weekday, data=train.raw)
+lm.model.casual.1 <- train(casual ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + hour + as.factor(weekday), data=train.raw, method="lm")
+rpart.model.registered.1 <- train(registered ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + year + month + weekday + hour, data=train.raw, method="rpart")
+rf.model.registered.1 <- randomForest(registered ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + year + month + weekday + hour, data=train.raw, method="rpart")
+#rpart.model1 <- train(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + hour + as.factor(weekday), data=train.raw, method="rpart")
+#rpart.model2 <- train(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + as.factor(combined), data=train.raw, method="rpart")
+#glm.model1 <- train(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + hour + as.factor(weekday), data=train.raw, method="glm")
+#glm.model2 <- train(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + combined, data=train.raw, method="glm")
+#glm.model3 <- glm(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + combined, data=train.raw, family="poisson")
+#glm.model4 <- glm(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + combined, data=train.raw, family="Gamma")
+#glm.model99 <- train(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + combined, data=train.raw, method = "lm", family="Gamma", preProcess=c("center", "scale"))
+
+#library(randomForest)
+#rf.model1 <- randomForest(count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed + hour + weekday, data=train.raw)
+
+gbm.casual.model <- train(casual ~ year + month + hour + weekday + season + holiday + workingday + weather + temp + atemp + humidity, data=train.raw, method="gbm")
+gbm.registered.model <- train(casual ~ year + month + hour + weekday + season + holiday + workingday + weather + temp + atemp + humidity, data=train.raw, method="gbm")
+
+
+#methods to test
+#blackboost  RMSE = 24.8 the longest
+#bstTree RMSE = 20.6 with mstop = 150, maxdepth = 3 and nu = 0.1
+#gbm (RMSE = 20.1 with shrinkage = 0,1 n.trees = 150 and interaction.depth = 3)
+#treebag RMSE = 24.7
+#avNNet didnt converge
+#bayesglm 36.1
+
+summary(model_test) #model_test
+#step(lm.model.casual.1,direction="both",test="F")
 
 #check RMSE
 library(qpcR)
@@ -61,6 +81,10 @@ RMSE(glm.model99)
 # glm2 = 88,7
 # glm3 = 4,75
 # glm4 = 0.50 # WE FOUND OUR WINNER
+
+#new checking basic Metrics:rmlse
+library(Metrics)
+rmlse(TRUTH,predicted)
 
 
 # lets made everything we did above for the test-dataset
@@ -84,6 +108,7 @@ test.raw$combined <- paste(as.character(test.raw$weekday), "_", as.character(tes
 test.raw$combined <- as.factor(test.raw$combined)
 
 #predict the counts
+lm.model.casual.predictions <- predict(gbm.casual.model, newdata = test.raw)
 glm.model3.prediction <- predict(glm.model99, newdata = test.raw)
 rf.model1.predicitions <- predict(rf.model1, newdata = test.raw)
 
